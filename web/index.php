271488cde1a8365fdb4bb36239331b060eb0de17
Arturo Aguila <agu09001@byui.edu>
2017-04-28 13:01:12 -0600

    testing

--- web/index.php
+++ web/index.php
@@ -5,6 +5,11 @@
         <title>Welcome to Art's CS 313 Homepage!</title>
         <!-- Bootstrap -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+        
         <!-- global stylesheet -->
         <link rel="stylesheet" href="main.css" />
     </head>
