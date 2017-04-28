52db675479c3668e59b07b95b80907630e6891e6
Arturo Aguila <agu09001@byui.edu>
2017-04-28 12:54:03 -0600

    added dropdown for links to assignments on my navbar

--- web/index.php
+++ web/index.php
@@ -19,21 +19,19 @@
         <!-- navbar -->
         <nav class="navbar navbar-default" role="navigation">
             <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar">dog</span>
-                        <span class="icon-bar">cat</span>
-                        <span class="icon-bar">mouse</span>
-                    </button>
-                    <a class="navbar-brand" href="#">brandbar?</a>
-                </div>
-
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="navbar">
                     <ul class="nav navbar-nav">
                         <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#">Assignments</a></li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">Assignments<span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Week 01</a></li>
+                                <li><a href="#">Week 02</a></li>
+                                <li><a href="#">Week 03</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">About Me</a></li>
                     </ul>
                 </div><!-- /.navbar-collapse -->
             </div><!-- /.container-fluid -->
