4ad9f285ffa96f80b28a56193e1013ffb7dce98b
Arturo Aguila <agu09001@byui.edu>
2017-04-28 11:59:08 -0600

    trying a background image

--- web/index.php
+++ web/index.php
@@ -3,3 +3,35 @@
   //phpinfo();
 
 ?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Welcome to Art's CS 313 Homepage!</title>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <!-- global stylesheet -->
+        <link rel="stylesheet" href="main.css" />
+
+    </head>
+
+    <body class="container">
+        <!-- header -->
+        <div class="row">
+          <div class="col-xs-12-12">
+              <img />
+          </div>
+        </div>
+        <!-- navbar -->
+
+        <!-- content - links to projects and old assignment -->
+
+
+
+
+
+
+    </body>
+</html>
