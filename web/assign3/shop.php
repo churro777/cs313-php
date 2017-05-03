c28d392ef084ff648558223da28f88f8d7f2f4ee
Arturo Aguila <agu09001@byui.edu>
2017-05-03 13:22:29 -0600

    testing

---
+++ web/assign3/shop.php
@@ -0,0 +1,54 @@
+<?php session_start() ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <link rel="stylesheet" href="a1.css">
+        <style media="screen">
+            .thing {
+                width: 100px;
+                height: 100px;
+                background-color: blue;
+            }
+        </style>
+    </head>
+    <body class="container">
+        <!-- Banner and Cart -->
+        <section class="row">
+            <!-- Banner -->
+            <div class="sidenav col-xs-9">
+
+            </div>
+
+            <!-- Cart -->
+            <div class="sidenav col-xs-3">
+
+            </div>
+
+        </section>
+
+        <!-- sidenav and content -->
+        <section class="row">
+            <!-- Shops in store -->
+            <div class="sidenav col-xs-2">
+
+            </div>
+
+            <!-- Shop -->
+            <div class="sidenav col-xs-10">
+                <!-- item -->
+                <div class="thing">
+
+                </div>
+
+            </div>
+
+        </section>
+
+
+
+
+
+    </body>
+</html>
