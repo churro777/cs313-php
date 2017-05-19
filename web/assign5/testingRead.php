3079c9341ef88bc0c5afb2ac3517cabfac97b976
Arturo Aguila <agu09001@byui.edu>
2017-05-19 10:32:10 -0600

    testing

--- web/assign5/testingRead.php
+++ web/assign5/testingRead.php
@@ -1,4 +1,18 @@
-<?php  ?>
+<?php
+$servername = "ec2-23-23-227-188.compute-1.amazonaws.com";
+$username = "mtfkybajvycpzz";
+$password = "1f9afc981c3b38e57fe02247902708c1fb4e0014721d9397d4b6a43dc8b1a809";
+$dbname = "dc3iv3q4t39i0c";
+
+// Create connection
+$conn = pg_connect("host=$servername port=5432 dbname=$dbname user=$username password=$password");
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$result = pg_query($conn, "select * from scriptures");
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,6 +20,6 @@
         <title>Assign 5 - Testing Read-only DB</title>
     </head>
     <body>
-        
+        <h1>Testing Data Retrieval</h1>
     </body>
 </html>
