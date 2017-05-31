d7ed74be66d6f2d4fc94b02eb738207c2faf1016
Arturo Aguila <agu09001@byui.edu>
2017-05-31 15:04:55 -0600

    updated

--- web/dndProject/php/authenticateUser.php
+++ web/dndProject/php/authenticateUser.php
@@ -6,27 +6,22 @@ $username = $_POST["username"];
 $password = $_POST["password"];
 
 if (isset($username) && isset($password)){
-echo "1";
+
     require 'connectToDb.php';
-    echo "2";
 
     $statement = $db->prepare('SELECT password FROM player WHERE username = :un;');
     $statement->bindParam(':un', $_POST["username"], PDO::PARAM_STR);
-    echo "3";
 
     $statement->execute();
     $passwordHash = $statement->fetch();
-    echo "4  ";
-echo $passwordHash[0] . "     ";
-    if(password_verify($password, $passwordHash[0]) ) {
-        echo $passwordHash[0];
 
-        echo "SUCCESS";
+    if(password_verify($password, $passwordHash[0]) ) {
+        $_SESSION["badLogin"] = "false";
+        $_SESSION["username"] = $_POST["username"];
+        header("Location: ../content.php");
     } else {
         $_SESSION["badLogin"] = "true";
-        echo "TERRIBLE";
     }
-    echo "5";
 
 
 }
