a422714101e7e3192b14d9e1f2e6b5aa1717cca4
Arturo Aguila <agu09001@byui.edu>
2017-05-31 14:57:16 -0600

    updated

--- web/dndProject/php/authenticateUser.php
+++ web/dndProject/php/authenticateUser.php
@@ -9,13 +9,13 @@ if (isset($username) && isset($password)){
 
     require 'connectToDb.php';
 
-    $statement = $db->prepare('SELECT password FROM player WHERE username = :un);');
+    $statement = $db->prepare('SELECT password FROM player WHERE username = :un;');
     $statement->bindParam(':un', $_POST["username"], PDO::PARAM_STR);
 
     $statement->execute();
     $passwordHash = $statement->fetch();
 
-    if(password_verify($password, $passwordHash[0]){
+    if(password_verify($password, $passwordHash[0]) ) {
         echo "SUCCESS";
     } else {
         $_SESSION["badLogin"] = "true";
