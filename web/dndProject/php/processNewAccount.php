5f2f1e2a49ddb98ad05124a0d94f2a7a5bbfad8a
Arturo Aguila <agu09001@byui.edu>
2017-05-31 14:32:59 -0600

    updated

--- web/dndProject/php/processNewAccount.php
+++ web/dndProject/php/processNewAccount.php
@@ -9,20 +9,20 @@ require 'connectToDb.php';
 
 require 'password.php';
 
-$password = htmlspecialchars($_POST['password']);
+$password = htmlspecialchars($_GET['password']);
 
 $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 echo $passwordHash;
 
-echo $_POST['password'];
-echo $_POST['firstName'];
-echo $_POST['lasttName'];
-echo $_POST['userName'];
+echo $_GET['password'];
+echo $_GET['firstName'];
+echo $_GET['lasttName'];
+echo $_GET['userName'];
 
 $statement = $db->prepare('INSERT INTO player(firstName, lastName, username, password) VALUES (:fn, :ln, :un, :ps);');
-$statement->bindParam(':fn', $_POST["firstName"], PDO::PARAM_STR);
-$statement->bindParam(':ln', $_POST["lasttName"], PDO::PARAM_STR);
-$statement->bindParam(':un', $_POST["userName"], PDO::PARAM_STR);
+$statement->bindParam(':fn', $_GET["firstName"], PDO::PARAM_STR);
+$statement->bindParam(':ln', $_GET["lasttName"], PDO::PARAM_STR);
+$statement->bindParam(':un', $_GET["userName"], PDO::PARAM_STR);
 $statement->bindParam(':ps', $passwordHash, PDO::PARAM_STR);
 
 $statement->execute();
