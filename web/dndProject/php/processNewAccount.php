7ec9de6e24b78542bd8fe99747820e9011efe799
Arturo Aguila <agu09001@byui.edu>
2017-05-31 13:16:25 -0600

    should work

--- web/dndProject/php/processNewAccount.php
+++ web/dndProject/php/processNewAccount.php
@@ -8,3 +8,10 @@ if ($_SESSION["newUser"] == "true"){
 require 'connectToDb.php';
 
 ?>
+
+<?php foreach ($db->query('select * from race') as $row): ?>
+    <label>
+        <input type="checkbox" name="topics" value="<?php echo $row[1] ?>"> <?php echo $row[1] ?>
+    </label>
+    <br>
+<?php endforeach; ?>
