24b62bebda454aa220a78a63a78489555142b5e2
Arturo Aguila <agu09001@byui.edu>
2017-05-31 13:08:03 -0600

    blargh

--- web/dndProject/php/connectToDb.php
+++ web/dndProject/php/connectToDb.php
@@ -1,10 +1,10 @@
 <?php
 
-$dbHost = 'localhost';
+$dbHost = 'ec2-23-23-227-188.compute-1.amazonaws.com';
 $dbPort = '5432';
-$dbName = 'test';
-$user = 'postgres';
-$pass = 'champ25';
+$dbName = 'dc3iv3q4t39i0c';
+$user = 'mtfkybajvycpzz';
+$pass = 'cham1f9afc981c3b38e57fe02247902708c1fb4e0014721d9397d4b6a43dc8b1a809p25';
 
 try {
     $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $user, $pass);
