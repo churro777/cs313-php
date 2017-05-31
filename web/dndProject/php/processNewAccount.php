8fd2a9e1802e8318626f91cddef8153711d2b344
Arturo Aguila <agu09001@byui.edu>
2017-05-31 14:19:57 -0600

    updated

--- web/dndProject/php/processNewAccount.php
+++ web/dndProject/php/processNewAccount.php
@@ -7,17 +7,18 @@ if ($_SESSION["newUser"] == "true"){
 
 require 'connectToDb.php';
 
+require 'password.php';
 
 $password = htmlspecialchars($_POST['password']);
 
 $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
 $statement = $db->prepare('INSERT INTO player(firstName, lastName, username, password) VALUES (:fn, :ln, :un, :ps)');
-// $statement->bindParam(':fn', $_POST["firstName"], PDO:PARAM_STR);
-// $statement->bindParam(':ln', $_POST["lasttName"], PDO:PARAM_STR);
-// $statement->bindParam(':un', $_POST["userName"], PDO:PARAM_STR);
-// $statement->bindParam(':un', $passwordHash, PDO:PARAM_STR);
-//
-// $statement->execute();
+$statement->bindParam(':fn', $_POST["firstName"], PDO:PARAM_STR);
+$statement->bindParam(':ln', $_POST["lasttName"], PDO:PARAM_STR);
+$statement->bindParam(':un', $_POST["userName"], PDO:PARAM_STR);
+$statement->bindParam(':un', $passwordHash, PDO:PARAM_STR);
+
+$statement->execute();
 
 ?>
