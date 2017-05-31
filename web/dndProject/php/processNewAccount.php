12c7ddb78720035e6b2df23004e12b6eb74c0ee6
Arturo Aguila <agu09001@byui.edu>
2017-05-31 14:18:16 -0600

    updated

--- web/dndProject/php/processNewAccount.php
+++ web/dndProject/php/processNewAccount.php
@@ -12,7 +12,7 @@ $password = htmlspecialchars($_POST['password']);
 
 $passwordHash = password_hash($password, PASSWORD_DEFAULT);
 
-// $statement = $db->prepare('INSERT INTO player(firstName, lastName, username, password) VALUES (:fn, :ln, :un, :ps)');
+$statement = $db->prepare('INSERT INTO player(firstName, lastName, username, password) VALUES (:fn, :ln, :un, :ps)');
 // $statement->bindParam(':fn', $_POST["firstName"], PDO:PARAM_STR);
 // $statement->bindParam(':ln', $_POST["lasttName"], PDO:PARAM_STR);
 // $statement->bindParam(':un', $_POST["userName"], PDO:PARAM_STR);
