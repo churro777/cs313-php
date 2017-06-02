5d7397d3d65bd43795fad33d40c2bb3a9ca1ecf1
Arturo Aguila <agu09001@byui.edu>
2017-06-02 17:38:57 -0600

    updated

--- web/dndProject/phpContent/getCharacters.php
+++ web/dndProject/phpContent/getCharacters.php
@@ -1,13 +1,13 @@
 <?php session_start();
 // get character names based off of the player
-$statement = $db->prepare('SELECT charactername FROM character
-                            WHERE player_id = (SELECT id FROM player WHERE username = :un);');
-
-$statement->bindParam(':un', $_SESSION["username"], PDO::PARAM_STR);
-
-$statement->execute();
-
-$characterNames = $statment->fetchAll();
+// $statement = $db->prepare('SELECT charactername FROM character
+//                             WHERE player_id = (SELECT id FROM player WHERE username = :un);');
+//
+// $statement->bindParam(':un', $_SESSION["username"], PDO::PARAM_STR);
+//
+// $statement->execute();
+//
+// $characterNames = $statment->fetchAll();
 ?>
 
 <?php foreach ($characterNames as $name): ?>
