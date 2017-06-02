585f6ce5add0fead31f503ddfb6d0022c2da9aa1
Arturo Aguila <agu09001@byui.edu>
2017-06-02 16:34:19 -0600

    updated

--- web/dndProject/php/createCharacter.php
+++ web/dndProject/php/createCharacter.php
@@ -6,11 +6,13 @@ echo $_POST["characterName"] . "<br />";
 echo $_POST["raceChoice"] . "<br />";
 echo $_POST["classChoice"] . "<br />";
 
-$statement = $db->prepare('INSERT INTO character (charactername, race_id, class_id, level, experience)
-                            VALUES (:cn,
+$statement = $db->prepare('INSERT INTO character (player_id, charactername, race_id, class_id, level, experience)
+                            VALUES ((SELECT id FROM player WHERE username = :un)
+                                    :cn,
                                     (SELECT id FROM race WHERE racename = :r),
                                     (SELECT id FROM race WHERE racename = :c),
                                     0, 0);');
+$statement->bindParam(':un', $_POST["username"], PDO::PARAM_STR);
 $statement->bindParam(':cn', $_POST["characterName"], PDO::PARAM_STR);
 $statement->bindParam(':r', $_POST["raceChoice"], PDO::PARAM_STR);
 $statement->bindParam(':c', $_POST["classChoice"], PDO::PARAM_STR);
