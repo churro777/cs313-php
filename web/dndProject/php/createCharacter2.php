87d90e86873e6d1ce29256acdedb2baf9dc8e713
Arturo Aguila <agu09001@byui.edu>
2017-06-03 12:14:00 -0600

    updated

--- web/dndProject/php/createCharacter2.php
+++ web/dndProject/php/createCharacter2.php
@@ -9,13 +9,15 @@ echo "INT - " . $_POST["int"] . "<br />";
 echo "WIS - " . $_POST["wis"] . "<br />";
 echo "CHA - " . $_POST["cha"] . "<br />";
 
+$STR = "STR";
+
 // prepare the insert statement
 $statement = $db->prepare('INSERT INTO abilityScores (character_id, type, score)
-                            VALUES ((SELECT id FROM character WHERE charactername = :cn), :score, :str);');
+                            VALUES ((SELECT id FROM character WHERE charactername = :cn), :type, :str);');
 echo "prepared <br />";
 // bind the variales to the corresponding item from the form on the previous page
 $statement->bindParam(':cn', $_SESSION["character"], PDO::PARAM_STR);
-$statement->bindParam(':score', "STR", PDO::PARAM_STR);
+$statement->bindParam(':type', $STR, PDO::PARAM_STR);
 $statement->bindParam(':str', $_POST["str"], PDO::PARAM_STR);
 echo "binded <br />";
 
@@ -25,6 +27,8 @@ try {
 } catch (PDOException $ex) {
     echo "Problem inserting character. Details: $ex";
 }
+
+echo "success!!";
 
 // send the user to the next creator page
 //header("Location: ../creator_2_scores.php");
