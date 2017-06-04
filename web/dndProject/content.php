7720b39c51d5d81a4fc24a0def3d1ba40e024537
Arturo Aguila <agu09001@byui.edu>
2017-06-03 23:44:06 -0600

    updated

--- web/dndProject/content.php
+++ web/dndProject/content.php
@@ -109,8 +109,8 @@ if (!isset($_SESSION["character"])) {
                             <?php require 'phpContent/getCharacterHitDice.php' ?>
                             <!-- ac -->
                             <div class="col-xs-4">
-                                <div class="row">
-                                    <div class="col-xs-5 center" style="font-style:20px;">AC</div>
+                                <div class="row space-2">
+                                    <div class="col-xs-5 center" style="font-size:20px;">AC</div>
                                     <input class="col-xs-5" type="text" name="ac" min="0">
                                 </div>
                             </div>
