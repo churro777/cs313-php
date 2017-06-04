3d8f24889c4a0da9a328a0999134753ed94f6801
Arturo Aguila <agu09001@byui.edu>
2017-06-04 02:11:19 -0600

    updated

--- web/dndProject/php/updateCharacter.php
+++ web/dndProject/php/updateCharacter.php
@@ -1,11 +1,12 @@
 <?php session_start();
 
     var_dump($_GET);
-
+    echo "before all teh sql stuff";
     $sql = 'UPDATE character
             SET :col = :val
             WHERE player_id = (SELECT id FROM player WHERE username = :un)
                 AND charactername = :c';
+    echo "before prepping";
     $statement = $db->prepare($sql);
     echo "prepped <br />";
     $statement->bindParam(':col', $_GET["column"], PDO::PARAM_STR);
