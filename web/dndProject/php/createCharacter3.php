1a276271665a3d087dbe619e32786fde1c8f3e6d
Arturo Aguila <agu09001@byui.edu>
2017-06-03 19:11:47 -0600

    updated

--- web/dndProject/php/createCharacter3.php
+++ web/dndProject/php/createCharacter3.php
@@ -6,57 +6,93 @@ echo "connected to db <br />";
 
 if (issset($_POST["Acrobatics"])) {
     echo $_POST["Acrobatics"] . "<br />";
+} else {
+    echo "Acrobatics";
 }
 if (issset($_POST["Animal Handling"])) {
     echo $_POST["Animal Handling"] . "<br />";
+} else {
+    echo "Animal Handling";
 }
 if (issset($_POST["Arcana"])) {
     echo $_POST["Arcana"] . "<br />";
+} else {
+    echo "Arcana";
 }
 if (issset($_POST["Athletics"])) {
     echo $_POST["Athletics"] . "<br />";
+} else {
+    echo "Athletics";
 }
 if (issset($_POST["Deception"])) {
     echo $_POST["Deception"] . "<br />";
+} else {
+    echo "Deception";
 }
 if (issset($_POST["History"])) {
     echo $_POST["History"] . "<br />";
+} else {
+    echo "History";
 }
 if (issset($_POST["Insight"])) {
     echo $_POST["Insight"] . "<br />";
+} else {
+    echo "Insight";
 }
 if (issset($_POST["Intimidation"])) {
     echo $_POST["Intimidation"] . "<br />";
+} else {
+    echo "Intimidation";
 }
 if (issset($_POST["Investigation"])) {
     echo $_POST["Investigation"] . "<br />";
+} else {
+    echo "Investigation";
 }
 if (issset($_POST["Medicine"])) {
     echo $_POST["Medicine"] . "<br />";
+} else {
+    echo "Medicine";
 }
 if (issset($_POST["Nature"])) {
     echo $_POST["Nature"] . "<br />";
+} else {
+    echo "Nature";
 }
 if (issset($_POST["Perception"])) {
     echo $_POST["Perception"] . "<br />";
+} else {
+    echo "Perception";
 }
 if (issset($_POST["Performance"])) {
     echo $_POST["Performance"] . "<br />";
+} else {
+    echo "Performance";
 }
 if (issset($_POST["Persuasion"])) {
     echo $_POST["Persuasion"] . "<br />";
+} else {
+    echo "Persuasion";
 }
 if (issset($_POST["Religion"])) {
     echo $_POST["Religion"] . "<br />";
+} else {
+    echo "Religion";
 }
 if (issset($_POST["Sleight of Hand"])) {
     echo $_POST["Sleight of Hand"] . "<br />";
+} else {
+    echo "Sleight of Hand";
 }
 if (issset($_POST["Stealth"])) {
     echo $_POST["Stealth"] . "<br />";
+} else {
+    echo "Stealth";
 }
 if (issset($_POST["Survival"])) {
     echo $_POST["Survival"] . "<br />";
+} else {
+    echo "Survival";
 }
 
 
