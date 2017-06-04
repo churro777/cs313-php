8170a3dee4edc938e54170b48eca2ebeee1f9e80
Arturo Aguila <agu09001@byui.edu>
2017-06-04 02:10:09 -0600

    updated

--- web/dndProject/js/saveData.js
+++ web/dndProject/js/saveData.js
@@ -6,25 +6,33 @@ function save(toBeSaved) {
             console.log("currentHitDice is going to updated now");
             var newInfo = document.getElementById('currentHitDice').value;
             console.log(newInfo);
-            location.assign("php/updateCharacter.php?column=currentHitDice&value=" + newInfo);
+            if (newInfo != "") {
+                location.assign("php/updateCharacter.php?column=currentHitDice&value=" + newInfo);
+            }
             break;
         case "maxHP":
             console.log("maxHP is going to updated now");
             var newInfo = document.getElementById('maxHP').value;
             console.log(newInfo);
-            location.assign("php/updateCharacter.php?column=maxHP&value=" + newInfo);
+            if (newInfo != "") {
+                location.assign("php/updateCharacter.php?column=maxHP&value=" + newInfo);
+            }
             break;
         case "currentHP":
             console.log("currentHP is going to updated now");
             var newInfo = document.getElementById('currentHP').value;
             console.log(newInfo);
-            location.assign("php/updateCharacter.php?column=currentHP&value=" + newInfo);
+            if (newInfo != "") {
+                location.assign("php/updateCharacter.php?column=currentHP&value=" + newInfo);
+            }
             break;
         case "ac":
             console.log("ac is going to updated now");
             var newInfo = document.getElementById('ac').value;
             console.log(newInfo);
-            location.assign("php/updateCharacter.php?column=ac&value=" + newInfo);
+            if (newInfo != "") {
+                location.assign("php/updateCharacter.php?column=ac&value=" + newInfo);
+            }
             break;
         default:
 
