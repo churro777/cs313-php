37663e65d1d19f63f345f8fe4e366134f0caf2f0
Arturo Aguila <agu09001@byui.edu>
2017-06-04 02:08:44 -0600

    updated

--- web/dndProject/php/updateCharacter.php
+++ web/dndProject/php/updateCharacter.php
@@ -7,16 +7,17 @@
             WHERE player_id = (SELECT id FROM player WHERE username = :un)
                 AND charactername = :c';
     $statement = $db->prepare($sql);
-
+    echo "prepped <br />";
     $statement->bindParam(':col', $_GET["column"], PDO::PARAM_STR);
     $statement->bindParam(':val', $_GET["value"], PDO::PARAM_STR);
     $statement->bindParam(':un', $_SESSION["username"], PDO::PARAM_STR);
     $statement->bindParam(':c', $_SESSION["character"], PDO::PARAM_STR);
-
+    echo "bound <br />";
     try {
         $statement->execute();
     } catch (PDOException $ex) {
         echo "Problem getting characters. Details: $ex";
     }
+    echo "success <br />";
 
 ?>
