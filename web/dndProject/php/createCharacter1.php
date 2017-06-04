5bea92d818cf24cf4bb1f558203b3880b9ea207d
Arturo Aguila <agu09001@byui.edu>
2017-06-04 01:59:11 -0600

    updated

--- web/dndProject/php/createCharacter1.php
+++ web/dndProject/php/createCharacter1.php
@@ -9,12 +9,12 @@ $_SESSION["class"] = $_POST["classChoice"];
 
 // prepare the insert statement
 $statement = $db->prepare('INSERT INTO character (player_id, charactername, race_id, class_id,
-                                                  level, experience, maxHP, currentHitDice, maxHP, currentHP,ac)
+                                                  level, experience, maxHP, currentHitDice, currentHP,ac)
                             VALUES ((SELECT id FROM player WHERE username = :un),
                                     :cn,
                                     (SELECT id FROM race WHERE racename = :r),
                                     (SELECT id FROM class WHERE classname = :c),
-                                    1, 1, 1, 1, 1, 1, 1);');
+                                    1, 1, 1, 1, 1, 1);');
 // bind the variales to the corresponding item from the form on the previous page
 $statement->bindParam(':un', $_SESSION["username"], PDO::PARAM_STR);
 $statement->bindParam(':cn', $_POST["characterName"], PDO::PARAM_STR);
