6479e6994e79f801f6201102ae12698166d11bef
Arturo Aguila <agu09001@byui.edu>
2017-06-04 01:40:36 -0600

    updated

--- web/dndProject/js/saveData.js
+++ web/dndProject/js/saveData.js
@@ -4,15 +4,23 @@ function save(toBeSaved) {
     switch (toBeSaved) {
         case "currentHitDice":
             console.log("currentHitDice is going to updated now");
+            var newInfo = document.getElementById('currentHitDice').value;
+            console.log(newInfo);
             break;
         case "maxHP":
             console.log("maxHP is going to updated now");
+            var newInfo = document.getElementById('maxHP').value;
+            console.log(newInfo);
             break;
         case "currentHP":
             console.log("currentHP is going to updated now");
+            var newInfo = document.getElementById('currentHP').value;
+            console.log(newInfo);
             break;
         case "ac":
             console.log("ac is going to updated now");
+            var newInfo = document.getElementById('ac').value;
+            console.log(newInfo);
             break;
         default:
 
