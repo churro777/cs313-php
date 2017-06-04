8003637ca7ff3e455ca3b876d5202a3f17e356a5
Arturo Aguila <agu09001@byui.edu>
2017-06-04 00:41:55 -0600

    updated

--- web/dndProject/js/saveData.js
+++ web/dndProject/js/saveData.js
@@ -1,9 +1,15 @@
-currentHitDice
-maxHp
-currentHP
-ac
 
 
+
+
+
+function character(name, currentHP, maxHP, currentHitDice, AC) {
+    this.name = name;
+    this.currentHP = currentHP;
+    this.maxHP = maxHP;
+    this.currentHitDice = currentHitDice;
+    this.AC = AC;
+}
 
 function save(toBeSaved){
 
@@ -12,5 +18,21 @@ function save(toBeSaved){
 
 function saveCharacter(){
     var character = document.getElementById('characterName').textContent;
+    var currentHitDice = document.getElementById('currentHitDice').value;
+    var maxHp = document.getElementById('maxHp').value;
+    var currentHP = document.getElementById('currentHP').value;
+    var AC = document.getElementById('AC').value;
+
     console.log(character);
+    console.log(currentHitDice);
+    console.log(maxHp);
+    console.log(currentHP);
+    console.log(AC);
+
+    var newguy = new character(character, )
+
+    // if (localStorage.characters.find())
+    // localStorage.characters.push(new character());
 }
+
+funciton isCharacter()
