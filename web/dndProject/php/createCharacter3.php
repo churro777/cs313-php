050de08e046b284f9b8e32458867bf989a5a7e4e
Arturo Aguila <agu09001@byui.edu>
2017-06-03 19:12:59 -0600

    updated

--- web/dndProject/php/createCharacter3.php
+++ web/dndProject/php/createCharacter3.php
@@ -4,92 +4,92 @@ echo "inside createCharacter3.php <br />";
 require 'connectToDb.php';
 echo "connected to db <br />";
 
-if (issset($_POST["Acrobatics"])) {
+if (isset($_POST["Acrobatics"])) {
     echo $_POST["Acrobatics"] . "<br />";
 } else {
     echo "Acrobatics";
 }
-if (issset($_POST["Animal Handling"])) {
+if (isset($_POST["Animal Handling"])) {
     echo $_POST["Animal Handling"] . "<br />";
 } else {
     echo "Animal Handling";
 }
-if (issset($_POST["Arcana"])) {
+if (isset($_POST["Arcana"])) {
     echo $_POST["Arcana"] . "<br />";
 } else {
     echo "Arcana";
 }
-if (issset($_POST["Athletics"])) {
+if (isset($_POST["Athletics"])) {
     echo $_POST["Athletics"] . "<br />";
 } else {
     echo "Athletics";
 }
-if (issset($_POST["Deception"])) {
+if (isset($_POST["Deception"])) {
     echo $_POST["Deception"] . "<br />";
 } else {
     echo "Deception";
 }
-if (issset($_POST["History"])) {
+if (isset($_POST["History"])) {
     echo $_POST["History"] . "<br />";
 } else {
     echo "History";
 }
-if (issset($_POST["Insight"])) {
+if (isset($_POST["Insight"])) {
     echo $_POST["Insight"] . "<br />";
 } else {
     echo "Insight";
 }
-if (issset($_POST["Intimidation"])) {
+if (isset($_POST["Intimidation"])) {
     echo $_POST["Intimidation"] . "<br />";
 } else {
     echo "Intimidation";
 }
-if (issset($_POST["Investigation"])) {
+if (isset($_POST["Investigation"])) {
     echo $_POST["Investigation"] . "<br />";
 } else {
     echo "Investigation";
 }
-if (issset($_POST["Medicine"])) {
+if (isset($_POST["Medicine"])) {
     echo $_POST["Medicine"] . "<br />";
 } else {
     echo "Medicine";
 }
-if (issset($_POST["Nature"])) {
+if (isset($_POST["Nature"])) {
     echo $_POST["Nature"] . "<br />";
 } else {
     echo "Nature";
 }
-if (issset($_POST["Perception"])) {
+if (isset($_POST["Perception"])) {
     echo $_POST["Perception"] . "<br />";
 } else {
     echo "Perception";
 }
-if (issset($_POST["Performance"])) {
+if (isset($_POST["Performance"])) {
     echo $_POST["Performance"] . "<br />";
 } else {
     echo "Performance";
 }
-if (issset($_POST["Persuasion"])) {
+if (isset($_POST["Persuasion"])) {
     echo $_POST["Persuasion"] . "<br />";
 } else {
     echo "Persuasion";
 }
-if (issset($_POST["Religion"])) {
+if (isset($_POST["Religion"])) {
     echo $_POST["Religion"] . "<br />";
 } else {
     echo "Religion";
 }
-if (issset($_POST["Sleight of Hand"])) {
+if (isset($_POST["Sleight of Hand"])) {
     echo $_POST["Sleight of Hand"] . "<br />";
 } else {
     echo "Sleight of Hand";
 }
-if (issset($_POST["Stealth"])) {
+if (isset($_POST["Stealth"])) {
     echo $_POST["Stealth"] . "<br />";
 } else {
     echo "Stealth";
 }
-if (issset($_POST["Survival"])) {
+if (isset($_POST["Survival"])) {
     echo $_POST["Survival"] . "<br />";
 } else {
     echo "Survival";
